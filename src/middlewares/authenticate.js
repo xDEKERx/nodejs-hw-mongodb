e5d21f88e330ae038477908eef77048549274416
Dmytro Deker <prostodeker@gmail.com>
2025-09-08 21:19:10 +0300

    edits

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -33,7 +33,6 @@ export const authenticate = async (req, res, next) => {
 
   if (isAccessTokenExpired) {
       next(createHttpError(401, 'Access token expired'));
-      return;
   }
 
   const user = await UsersCollection.findById(session.userId);
